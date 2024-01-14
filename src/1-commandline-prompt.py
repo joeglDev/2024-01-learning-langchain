c0620756ba38876e7b2d467b8f9982143e99491b
Hiroji <joegilbertdev@outlook.com>
2024-01-14 16:49:53 +0000

    1-commandline
    Output parser

--- src/1-commandline-prompt.py
+++ src/1-commandline-prompt.py
@@ -1,4 +1,5 @@
 from langchain_community.llms import Ollama
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 # Using orca-mini for testing purposes as fast.
@@ -29,4 +30,19 @@ def get_response_from_prompt_with_system_prompt() -> str:
     return response
 
 
-print(get_response_from_prompt_with_system_prompt())
+def response_with_output_parser() -> str:
+    """Output parser formats the streamed output of the LLM e.g. as a string"""
+    output_parser = StrOutputParser()
+
+    query: str = input("Please input a prompt here:")
+    prompt_with_system_prompt = ChatPromptTemplate.from_messages(
+        [("system", "You are the grey jedi Anakin Skywalker."), ("user", "{input}")]
+    )
+
+    chain = prompt_with_system_prompt | llm | output_parser
+    response: str = chain.invoke({"input": f"{query}"})
+
+    return response
+
+
+print(response_with_output_parser())

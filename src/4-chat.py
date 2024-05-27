238b460854e6c1cf68bdd7a634c87427277ad1a2
Joe Gilbert <joegilbertdev@outlook.com>
2024-05-27 13:44:07 +0100

    chatbot
    Moved chatbot run script to chatbot method

--- src/4-chat.py
+++ src/4-chat.py
@@ -13,22 +13,5 @@ system_prompt = ChatPromptTemplate.from_messages(
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
-chat = Chatbot(model=Ollama(model="mistral"), system=system_prompt)
-
-CONTINUE_CHAT = True
-
-print("Type 'goodbye' to end the chat.")
-
-while CONTINUE_CHAT:
-    prompt = input("Please enter your prompt:")
-    if prompt.strip().lower() != "goodbye":
-        completion = chat.get_completion(prompt)
-        print(f"Output: {completion}")
-
-    else:
-        CONTINUE_CHAT = False
-        log = chat.get_history()
-        print("The chat has ended.")
-        print("Chat log:")
-        print(f"Length of chat: {len(log)}")
-        print(log)
+chat = Chatbot(model=Ollama(model="orca-mini"), system=system_prompt)
+chat.run()
--- src/classes/chatbot.py
+++ src/classes/chatbot.py
@@ -1,6 +1,6 @@
+from langchain_community.chat_message_histories.in_memory import ChatMessageHistory
 from langchain_community.llms.ollama import Ollama
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.memory import ChatMessageHistory
 
 
 class Chatbot:
@@ -8,6 +8,7 @@ class Chatbot:
         self.Model = model
         self.System = system
         self.History: list[dict[str, str]] = ChatMessageHistory()
+        self.continue_chat = True
 
     def get_completion(self, prompt: str):
         """Gets a basic chat completion with chat history"""
@@ -26,3 +27,20 @@ class Chatbot:
     def get_history(self) -> list[dict[str, str]]:
         """Returns chat history"""
         return self.History.messages
+
+    def run(self):
+        print("Type 'goodbye' to end the chat.")
+
+        while self.continue_chat:
+            prompt = input("Please enter your prompt:")
+            if prompt.strip().lower() != "goodbye":
+                completion = self.get_completion(prompt)
+                print(f"Output: {completion}")
+
+            else:
+                self.continue_chat = False
+                log = self.get_history()
+                print("The chat has ended.")
+                print("Chat log:")
+                print(f"Length of chat: {len(log)}")
+                print(log)

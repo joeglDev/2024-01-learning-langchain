e0756c3593336ba69c3885202f5bea180136ec4f
Joe Gilbert <joegilbertdev@outlook.com>
2024-05-27 19:11:58 +0100

    7-speech-chatbot
    Added basic speech implementation

--- src/7-speaking-chatbot.py
+++ src/7-speaking-chatbot.py
@@ -2,17 +2,16 @@ from langchain_community.llms.ollama import Ollama
 from langchain_core.prompts import MessagesPlaceholder, ChatPromptTemplate
 
 from src.classes.chatbots.SpeakingChatbot import SpeakingChatbot
-from src.classes.chatbots.prompts import SYX_SYSTEM_PROMPT
 
 # setup large language model parameters
 system_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            SYX_SYSTEM_PROMPT,
+            "You are not a helpful ai.",
         ),
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
-chat = SpeakingChatbot(model=Ollama(model="mistral"), system=system_prompt)
+chat = SpeakingChatbot(model=Ollama(model="syx"), system=system_prompt)
 chat.run()
--- src/classes/chatbots/SpeakingChatbot.py
+++ src/classes/chatbots/SpeakingChatbot.py
@@ -1,5 +1,9 @@
+import torch
+from TTS.api import TTS
+
 from langchain_community.llms.ollama import Ollama
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
+from playsound import playsound
 
 from src.classes.chatbots.chatbot import Chatbot
 
@@ -8,3 +12,46 @@ class SpeakingChatbot(Chatbot):
     def __init__(self, model: Ollama, system: ChatPromptTemplate):
         super().__init__(model, system)
 
+    def speak_completion(self, completion: str):
+        # todo convert this funct into a dedicated TTS class handler
+
+        # Get device
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # Init TTS
+        tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+        # Run TTS
+        # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+        # Text to speech list of amplitude values as output
+        print("Generating voice")
+        wav = tts.tts(text=completion, speaker="Ana Florence", language="en")
+        # todo convert list of amplitude values into .wav and play audio
+        # Play the generated audio file
+        # Generate speech by cloning a voice using default settings
+        wav = tts.tts_to_file(
+            text=completion,
+            file_path="./output.wav",
+            speaker="Ana Florence",
+            language="en",
+            split_sentences=True,
+        )
+
+        # play audio from file
+        playsound("./output.wav")
+
+    def get_completion(self, prompt: str):
+        """Gets a basic chat completion with chat history"""
+
+        print("Thinking...")
+        chain = self.System | self.Model
+        self.History.add_user_message(prompt)
+
+        model_input = {"messages": self.History.messages}
+
+        completion: str = chain.invoke(model_input)
+        self.History.add_ai_message(completion)
+
+        self.speak_completion(completion)
+
+        return completion
